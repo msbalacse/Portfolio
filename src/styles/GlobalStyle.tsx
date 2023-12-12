d07d1b79b2e5dba8828f85b7c036bb29de344dc0
Balamurugan <msbalacse@gmail.com>
2023-12-12 23:59:03 +0530

    twin marc with tailwindcss

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
+
+const GlobalStyle = () => {
+  return (
+    <>
+      <BaseStyles />
+    </>
+  );
+};
+
+export default GlobalStyle;
