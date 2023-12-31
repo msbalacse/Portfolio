4d889c7fd46a95cfad0682b0749b8c1a456c136e
Balamurugan <msbalacse@gmail.com>
2023-12-31 23:36:27 +0530

    image import

--- src/Components/ProfileDetail.tsx
+++ src/Components/ProfileDetail.tsx
@@ -1,16 +1,19 @@
-import { ProfilePic } from "../assets/images";
+import ProfilePic from '../assets/images/ProfilePic.jpg';
 
 const ProfileDetail = () => {
-    return (
-        <div className="flex gap-4 p-4">
-            <img src={ProfilePic} className="w-[150px]" alt="profile picture" />
-            <div>
-                <p className="text-4xl font-bold text-red-600">Balamurugan</p>
-                <p className="text-xs">BE Computer Science Engineer</p>
-                <p className={"mt-3"}>Hello! I am Balamurugan, a front-end developer and passionate React enthusiast from the vibrant city of Madurai, Tamilnadu.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex gap-4 p-4">
+      <img src={ProfilePic} className="w-[150px]" alt="profile picture" />
+      <div>
+        <p className="text-4xl font-bold text-red-600">Balamurugan</p>
+        <p className="text-xs">BE Computer Science Engineer</p>
+        <p className={'mt-3'}>
+          Hello! I am Balamurugan, a front-end developer and passionate React
+          enthusiast from the vibrant city of Madurai, Tamilnadu.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileDetail;
--- src/assets/images/index.js
+++
@@ -1,3 +0,0 @@
-import ProfilePic from './ProfilePic.jpg';
-
-export { ProfilePic };
